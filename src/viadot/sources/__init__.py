262de125a02c5c886c553c4be1b533ab1a822878
Diego-H-S <dhidalgosoto@dyvenia.com>
2024-08-28 08:51:44 +0200

    📝 added sap base structure to init files.

--- src/viadot/sources/__init__.py
+++ src/viadot/sources/__init__.py
@@ -9,22 +9,23 @@ from .genesys import Genesys
 from .hubspot import Hubspot
 from .mindful import Mindful
 from .outlook import Outlook
+from .sap_bw import Sapbw
 from .sharepoint import Sharepoint
 from .sql_server import SQLServer
 from .trino import Trino
 from .uk_carbon_intensity import UKCarbonIntensity
 
-
 __all__ = [
     "CloudForCustomers",
     "ExchangeRates",
     "Genesys",
-    "Outlook",
     "Hubspot",
     "Mindful",
+    "Outlook",
+    "Sapbw",
     "Sharepoint",
-    "Trino",
     "SQLServer",
+    "Trino",
     "UKCarbonIntensity",
 ]
 
