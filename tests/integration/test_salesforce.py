2aac697941624c2246a4d9b1ce0bc960f2c81fa3
Diego-H-S <dhidalgosoto@dyvenia.com>
2024-08-29 11:20:55 +0200

    ✨ added unit test file.

---
+++ tests/integration/test_salesforce.py
@@ -0,0 +1,14 @@
+from viadot.orchestration.prefect.flows import salesforce_to_adls
+
+
+def test_salesforce_to_adls(salesforce_config_key, adls_credentials_secret):
+    state = salesforce_to_adls(
+        azure_key_vault_secret=salesforce_config_key,
+        env="dev",
+        table="Contact",
+        adls_path="raw/dyvenia_sandbox/salesforce/salesforce.csv",
+        adls_azure_key_vault_secret="app-azure-cr-datalakegen2-dev",
+        adls_path_overwrite=True,
+    )
+    all_successful = all(s.type == "COMPLETED" for s in state)
+    assert all_successful, "Not all tasks in the flow completed successfully."
